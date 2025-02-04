bbe5b487402cc9e53a0d8e34f6ccbe14a2d480ec
Artur <147540923+arturTheDev17@users.noreply.github.com>
2025-02-04 19:58:36 -0300

    Reapply "api estruturada deve estar pronta"
    This reverts commit a20565669af61ce87f77d093d384c3e24ddd37c1.

--- APIs_Estruturadas/Images_Users_API/API/Service/ImageService.js
+++ APIs_Estruturadas/Images_Users_API/API/Service/ImageService.js
@@ -1,20 +1,45 @@
-const imageRepository = require('../repository/imageRepository');
-
+const imageRepository = require("../Repository/ImagesRepository");
+const Image = require("../Model/Image");
 class ImageService {
   async getImages() {
-    return await imageRepository.listarImagens();
+    const images = await imageRepository.listarImagens();
+    const imagesReturn = images.map((image) => {
+      return new Image(
+        image.id,
+        image.titulo,
+        image.referencia,
+        image.data_criacao
+      );
+    });
+    return imagesReturn;
   }
 
-  async createImage(image) {
-    return await imageRepository.createImage(image);
+  async createImage(image = {}) {
+    const newImage = await imageRepository.createImage(
+      new Image(image.id, image.titulo, image.referencia, image.data_criacao)
+    );
+    return new Image(
+      newImage.id,
+      newImage.titulo,
+      newImage.referencia,
+      newImage.data_criacao
+    );
   }
 
   async getImage(id) {
-    return await imageRepository.getImage(id);
+    const image = await imageRepository.getImage(id);
+    const imageReturn = new Image(
+      image.id,
+      image.titulo,
+      image.referencia,
+      image.data_criacao
+    );
+    return imageReturn;
   }
 
   async updateImage(id, image) {
-    return await imageRepository.updateImage(id, image);
+    const retorno = await imageRepository.updateImage(id, image);
+    return retorno >= 1 ? "Image updated" : "Image not found";
   }
 
   async deleteImage(id) {
